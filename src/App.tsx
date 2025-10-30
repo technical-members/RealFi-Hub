85746001e9a94c69cab8337ef54b4cbb09274b63
mihalo3c <mihalo3c@gmail.com>
2025-10-30 11:30:37 +0000

    Refactor logic in App component

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { Web3Provider } from "@/providers/Web3Provider";
+import Index from "./pages/Index";
+import Marketplace from "./pages/Marketplace";
+import Dashboard from "./pages/Dashboard";
+import Investors from "./pages/Investors";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
+
+const App = () => (
+  <HelmetProvider>
+    <Web3Provider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/investors" element={<Investors />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </Web3Provider>
+  </HelmetProvider>
+);
+
+export default App;
